4901c4e94042c60bdc8296623fe4a8ed21d07ba1
michaeldai1006 <dtc74110@gmail.com>
2020-04-16 23:34:24 -0400

    Initial Commit

---
+++ TDSwiftAutoAlignmentTextView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDSwiftAutoAlignmentTextView
+//
+//  Created by Michael Dai on 4/16/20.
+//  Copyright © 2020 TDSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
